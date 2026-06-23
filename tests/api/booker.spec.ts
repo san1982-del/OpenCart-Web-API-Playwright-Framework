051c507a49cd54f3c190601e19ae8b06e6d5a36c
--san1982-del <-yuvishavi2906@gmail.com>
2026-06-23 16:12:52 -0400

    playwright config file updated

--- tests/api/booker.spec.ts
+++ tests/api/booker.spec.ts
@@ -1,7 +1,7 @@
 
 import { test, expect } from '../../src/fixtures/apifixtures';
 
-const TOKEN = process.env.API_TOKEN!;
+const TOKEN = process.env.GOREST_API_TOKEN!;
 let AUTH_HEADER = { Authorization: `Bearer ${TOKEN}` };
 
 
